b7f5f8df841f3812833bdb3ed238a4387246e59f
asamaiya00 <asamaiya00@gmail.com>
2020-08-07 00:47:59 +0530

    find duplicates in an array O(1)

--- GFG/Arrays/findDuplicates.cpp
+++ GFG/Arrays/findDuplicates.cpp
@@ -21,6 +21,20 @@ int main()
 }
 // } Driver Code Ends
 
+// by changing the array 
+
+// vector<int> findDuplicates(vector<int>& nums) {
+//      vector<int> ans;
+//      for (int i = 0; i < nums.size(); i++) {
+//          int n = abs(nums[i]);
+//          if(nums[n - 1] < 0)
+//              ans.push_back(n);
+//          nums[n - 1] *= -1;      
+//      }
+//         return ans;
+//     }
+
+
 vector<int> duplicates(int a[], int size)
 {
     vector<int> ans;
