9ae6948ca8cd5465a54e4eb8c167cb65a0761afa
asamaiya00 <asamaiya00@gmail.com>
2020-08-20 11:59:02 +0530

    added xor method for missing number

--- GFG/Arrays/findMissing.cpp
+++ GFG/Arrays/findMissing.cpp
@@ -12,12 +12,23 @@ int main()
         int a[n];
         for (int i = 0; i < n; i++)
             cin >> a[i];
-        int sum = 0;
-        for (int i = 0; i < n; i++)
-            sum += a[i];
-        n++;
-        n = n * (n + 1) / 2;
-        cout << n - sum << endl;
+
+// using sum of natural numbers
+        // int sum = 0;
+        // for (int i = 0; i < n; i++)
+        //     sum += a[i];
+        // n++;
+        // n = n * (n + 1) / 2;
+        // cout << n - sum << endl;
+
+// using xor
+        int x1 = a[0];
+        int x2 = 1;
+        for (int i = 1; i < n; i++)
+            x1 ^= a[i];
+        for (int i = 2; i <= n + 1; i++)
+            x2 ^= i;
+        cout << (x1 ^ x2) << endl;
     }
     return 0;
 }
