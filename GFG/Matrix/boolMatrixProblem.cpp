9196790f6e23c1fcc755de0a91a9c698a12b741d
asamaiya00 <asamaiya00@gmail.com>
2020-08-25 17:47:30 +0530

    Boolean Matrix Problem O(1)

--- GFG/Matrix/boolMatrixProblem.cpp
+++ GFG/Matrix/boolMatrixProblem.cpp
@@ -14,6 +14,8 @@ int main()
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
                 cin >> mat[i][j];
+
+
         vector<int> row(n), col(m);
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
@@ -29,6 +31,32 @@ int main()
                     mat[i][j] = 1;
                 }
 
+
+// O(1) solution
+
+        int isCol = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (mat[i][0] == 1)
+                isCol = 1;
+            for (int j = 1; j < m; j++)
+                if (mat[i][j] == 1)
+                {
+                    mat[i][0] = 1;
+                    mat[0][j] = 1;
+                }
+        }
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = m - 1; j >= 1; j--)
+                if (mat[i][0] == 1 || mat[0][j] == 1)
+                    mat[i][j] = 1;
+            if (isCol == 1)
+                mat[i][0] = 1;
+        }
+
+// print
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
