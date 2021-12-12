f7685cf81c50b6a62ce562c0c6752974a7cc911c
Animesh Samaiya <asamaiya00@gmail.com>
2021-12-13 00:51:39 +0530

    Find n/k th node in Linked list

---
+++ GFG/Linked Lists/fractionalNode.cpp
@@ -0,0 +1,77 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int fractional_node(struct Node *head, int k);
+
+struct Node {
+  int data;
+  struct Node *next;
+
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+
+};
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    Node *ptr, *start = NULL, *ptr1;
+    int n, i;
+    cin >> n;
+
+    for (i = 0; i < n; i++) {
+      int value;
+      cin >> value;
+
+      ptr = new Node(value);
+
+      if (start == NULL) {
+        start = ptr;
+        ptr1 = ptr;
+      } else {
+        ptr1 -> next = ptr;
+        ptr1 = ptr1 -> next;
+      }
+    }
+    ptr1 -> next = NULL;
+    int k;
+    cin >> k;
+    int p = fractional_node(start, k);
+    cout << p << endl;
+  }
+}
+// } Driver Code Ends
+
+/*The structure of the node is
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+int fractional_node(struct Node *head, int k) {
+  // your code here
+  struct Node *temp = head;
+  double n = 0, data = 0;
+  while (temp) {
+    n++;
+    temp = temp -> next;
+  }
+  n = ceil(n / k);
+  for (int i = 0; i < n; i++) {
+    data = head -> data;
+    head = head -> next;
+  }
+  return data;
+}
