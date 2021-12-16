2b704fa5a2b6a0c564cb9605e5458a549e8c4e42
Animesh Samaiya <asamaiya00@gmail.com>
2021-12-16 23:15:26 +0530

    Left View of Binary Tree

--- GFG/Trees/leftViewOfBinaryTree.cpp
+++ GFG/Trees/leftViewOfBinaryTree.cpp
@@ -1,110 +1,106 @@
+// { Driver Code Starts
 #include <bits/stdc++.h>
+
 using namespace std;
 
 // Tree Node
-struct Node
-{
-    int data;
-    Node *left;
-    Node *right;
+struct Node {
+  int data;
+  Node * left;
+  Node * right;
 };
 
-void leftView(struct Node *root);
+vector <int> leftView(struct Node * root);
 
 // Utility function to create a new Tree Node
-Node *newNode(int val)
-{
-    Node *temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
+Node * newNode(int val) {
+  Node * temp = new Node;
+  temp -> data = val;
+  temp -> left = NULL;
+  temp -> right = NULL;
 
-    return temp;
+  return temp;
 }
 
 // Function to Build Tree
-Node *buildTree(string str)
-{
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
+Node * buildTree(string str) {
+  // Corner Case
+  if (str.length() == 0 || str[0] == 'N')
+    return NULL;
 
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
+  // Creating vector of strings from input
+  // string after spliting by space
+  vector <string> ip;
 
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
+  istringstream iss(str);
+  for (string str; iss >> str;)
+    ip.push_back(str);
 
-    // for(string i:ip)
-    //     cout<<i<<" ";
-    // cout<<endl;
-    // Create the root of the tree
-    Node *root = newNode(stoi(ip[0]));
+  // for(string i:ip)
+  //     cout<<i<<" ";
+  // cout<<endl;
+  // Create the root of the tree
+  Node * root = newNode(stoi(ip[0]));
 
-    // Push the root to the queue
-    queue<Node *> queue;
-    queue.push(root);
+  // Push the root to the queue
+  queue < Node * > queue;
+  queue.push(root);
 
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size())
-    {
+  // Starting from the second element
+  int i = 1;
+  while (!queue.empty() && i < ip.size()) {
 
-        // Get and remove the front of the queue
-        Node *currNode = queue.front();
-        queue.pop();
+    // Get and remove the front of the queue
+    Node * currNode = queue.front();
+    queue.pop();
 
-        // Get the current node's value from the string
-        string currVal = ip[i];
+    // Get the current node's value from the string
+    string currVal = ip[i];
 
-        // If the left child is not null
-        if (currVal != "N")
-        {
+    // If the left child is not null
+    if (currVal != "N") {
 
-            // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
+      // Create the left child for the current node
+      currNode -> left = newNode(stoi(currVal));
 
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N")
-        {
-
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
+      // Push it to the queue
+      queue.push(currNode -> left);
     }
 
-    return root;
+    // For the right child
+    i++;
+    if (i >= ip.size())
+      break;
+    currVal = ip[i];
+
+    // If the right child is not null
+    if (currVal != "N") {
+
+      // Create the right child for the current node
+      currNode -> right = newNode(stoi(currVal));
+
+      // Push it to the queue
+      queue.push(currNode -> right);
+    }
+    i++;
+  }
+
+  return root;
 }
 
-int main()
-{
-    int t;
-    scanf("%d ", &t);
-    while (t--)
-    {
-        string s;
-        getline(cin, s);
-        Node *root = buildTree(s);
-        leftView(root);
-        cout << endl;
-    }
-    return 0;
+int main() {
+  int t;
+  scanf("%d ", & t);
+  while (t--) {
+    string s;
+    getline(cin, s);
+    Node * root = buildTree(s);
+    vector <int> vec = leftView(root);
+    for (int x: vec)
+      cout << x << " ";
+    cout << endl;
+  }
+  return 0;
 }
 
 // } Driver Code Ends
@@ -123,22 +119,24 @@ struct Node
     }
 };
  */
-void leftViewUtil(Node *root, int curr, int *max_level)
-{
-    if (!root)
-        return;
-    if (*max_level < curr)
-    {
-        cout << root->data << " ";
-        *max_level = curr;
-    }
-    leftViewUtil(root->left, curr + 1, max_level);
-    leftViewUtil(root->right, curr + 1, max_level);
+
+//Function to return a list containing elements of left view of the binary tree.
+static int maxHeight = -1;
+void leftViewHelper(Node *node, vector <int> &ans, int height) {
+  if (!node) return;
+  if (height > maxHeight) {
+    maxHeight = height;
+    ans.push_back(node -> data);
+  }
+
+  leftViewHelper(node -> left, ans, height + 1);
+  leftViewHelper(node -> right, ans, height + 1);
 }
-// A wrapper over leftViewUtil()
-void leftView(Node *root)
-{
-    int max_level = 0;
-    leftViewUtil(root, 1, &max_level);
-    // Your code here
+vector <int> leftView(Node *root) {
+  // Your code here
+  vector <int> ans;
+  maxHeight = -1;
+  ans.clear();
+  leftViewHelper(root, ans, 0);
+  return ans;
 }
