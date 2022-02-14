872ac08aeb46beb37c7888d6258b6c8d269b352d
Animesh Samaiya <asamaiya00@gmail.com>
2022-02-14 22:58:07 +0530

    Find Missing And Repeating

--- GFG/Arrays/findRepeatingAndMissingElement.cpp
+++ GFG/Arrays/findRepeatingAndMissingElement.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+int *findTwoElement(int *arr, int n) {
+	int *ans = new int[2];
+	for (int i = 0; i < n; i++)	{
+		int index = abs(arr[i]) - 1;
+		if (arr[index] < 0)
+			ans[0] = index + 1;
+		else
+			arr[index] = -arr[index];
+	}
+	for (int i = 0; i < n; i++)
+		if (arr[i] > 0){
+			ans[1] = i + 1;
+            break;
+        }
+	return ans;
+}
+
 void repeatingAndMissing()
 {
     int n;
